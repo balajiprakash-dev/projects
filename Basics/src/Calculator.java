787f9638348ff2d539b417a85201d132186f901e
Balaji Prakash <balaji17prakash@gmail.com>
2022-09-27 18:04:26 +0530

    Add basic calculator class and junit test for the same

---
+++ Basics/src/Calculator.java
@@ -0,0 +1,22 @@
+class Calculator
+{
+    static int add(int input1, int input2)
+    {
+        return input1 + input2;
+    }
+
+    static int subtract(int input1, int input2)
+    {
+        return input1 - input2;
+    }
+
+    static int multiply(int input1, int input2)
+    {
+        return input1 * input2;
+    }
+
+    static int divide(int input1, int input2)
+    {
+        return input1 / input2;
+    }
+}
